45c23419a9bf8387522ba4856700346607f6440f
Vijayabaskar <vj2k02@gmail.com>
2025-03-15 23:40:02 +0530

    feat: style clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { TanStackField, injectForm } from '@tanstack/angular-form';
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+ selector: 'app-root',
+ imports: [RouterOutlet, TanStackField],
+ templateUrl: './app.component.html',
+ styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-starter-kit';
+ title = 'angular-starter-kit';
 }
