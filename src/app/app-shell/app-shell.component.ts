9ab6db4cb7e691f3d49cbdda5462e03dd92ab198
Vijayabaskar <vj2k02@gmail.com>
2025-03-15 23:50:33 +0530

    feat: app shell added

---
+++ src/app/app-shell/app-shell.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+ selector: 'app-app-shell',
+ imports: [RouterOutlet],
+ templateUrl: './app-shell.component.html',
+ styleUrl: './app-shell.component.css'
+})
+export class AppShellComponent {
+ loading = false;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TanStackField, injectForm } from '@tanstack/angular-form';
+import type { LoadingService } from './services/loading.service';
 @Component({
  selector: 'app-root',
  imports: [RouterOutlet, TanStackField],
@@ -9,4 +10,7 @@ import { TanStackField, injectForm } from '@tanstack/angular-form';
 })
 export class AppComponent {
  title = 'angular-starter-kit';
+ constructor(private loadingService: LoadingService) {
+
+ }
 }
